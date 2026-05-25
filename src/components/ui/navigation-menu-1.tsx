59000c5ab542f9265ea3d0e0758dee543a075d49
cmkpriyashi <cmk.priyanshi@gmail.com>
2026-05-25 13:52:31 +0530

    fix: resolve prerender build errors by marking desktop nav and menu primitives as Client Components

--- src/components/ui/navigation-menu-1.tsx
+++ src/components/ui/navigation-menu-1.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from 'react';
 import { cn } from '@/lib/utils';
 import { NavigationMenu as NavigationMenuPrimitive } from '@base-ui-components/react/navigation-menu';
